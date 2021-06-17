a724e1292c9d2b561fca0f33e2c6cbac1cb3abab
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-17 16:34:32 -0400

    Initial Commit

---
+++ JSONDataPractice/JSONDataPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONDataPracticeApp.swift
+//  JSONDataPractice
+//
+//  Created by Vivek Pattanaik on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct JSONDataPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
